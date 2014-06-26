1feafae61c7f8e00b06a51cab70528fee34e7b65
Tony Coconate <me@tonycoconate.com>
2014-06-26 11:04:29 -0500

    release

--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -1,19 +1,19 @@
 ActiveRecord::Schema.define do
   self.verbose = false
 
-  create_table :non_pusherable_models, :force => true do |t|
+  create_table :non_pusherable_models, force: true do |t|
     t.timestamps
   end
 
-  create_table :defaulted_pusherable_models, :force => true do |t|
+  create_table :defaulted_pusherable_models, force: true do |t|
     t.timestamps
   end
 
-  create_table :pusherable_models, :force => true do |t|
+  create_table :pusherable_models, force: true do |t|
     t.timestamps
   end
 
-  create_table :callable_pusherable_models, :force => true do |t|
+  create_table :callable_pusherable_models, force: true do |t|
     t.string "name"
     t.timestamps
   end
